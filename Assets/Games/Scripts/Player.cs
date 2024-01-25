979cff79a277e9d8538cdfff932e03c25933a1c8
datlee240301 <devenewefall@gmail.com>
2024-01-25 13:35:07 +0700

    + teleport

--- Assets/Games/Scripts/Player.cs
+++ Assets/Games/Scripts/Player.cs
@@ -18,6 +18,8 @@ namespace io.lockedroom.Games.Jackal {
         /// animator tank
         /// </summary>
         private Animator Tank;
+        public Vector2 distanceToMoveX;
+        public Vector2 distanceToMoveY;
         // Start is called before the first frame update
         void Start() {
             Tank = GetComponent<Animator>();
@@ -27,15 +29,31 @@ namespace io.lockedroom.Games.Jackal {
             MoveInput.x = Input.GetAxis("Horizontal");
             MoveInput.y = Input.GetAxis("Vertical");
             transform.position += MoveInput * MoveSpeed * Time.deltaTime;
+            Telepoort();
             Animate();
         }
+        private void Telepoort() {
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                Vector2 currentPos = transform.position;
+
+                if (Input.GetKey(KeyCode.D))
+                    transform.position = currentPos + distanceToMoveX;
+                else if (Input.GetKey(KeyCode.A))
+                    transform.position = currentPos - distanceToMoveX;
+                else if (Input.GetKey(KeyCode.W))
+                    transform.position = currentPos + distanceToMoveY;
+                else if (Input.GetKey(KeyCode.S))
+                    transform.position = currentPos - distanceToMoveY;
+            }
+        }
+
         private void OnCollisionEnter2D(Collision2D collision) {
             if (collision.gameObject.CompareTag("Soldier")) {
                 collision.gameObject.SetActive(false);
-            }else if (collision.gameObject.CompareTag("SoldierBullet")) {
+            } else if (collision.gameObject.CompareTag("SoldierBullet")) {
                 gameObject.SetActive(false);
                 SceneManager.LoadScene("Lv1");
-            } 
+            }
         }
         private void Animate() {
             Tank.SetFloat("MovementX", MoveInput.x);
